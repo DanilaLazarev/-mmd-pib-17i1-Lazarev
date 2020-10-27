02773070f4245920ca8e06afda9409bf15a91dc0
Danila <danlaz55rus@gmail.com>
2020-10-27 10:36:16 +0600

    Small change in Patient

--- task4/src/PatientCard.java
+++ task4/src/PatientCard.java
@@ -1,18 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class PatientCard implements Human {
 
     private final String name;
-    private final List<Visit> visits;
 
     public PatientCard(String name) {
         this.name = name;
-        this.visits = new ArrayList<>();
-    }
-
-    public List<Visit> getVisits() {
-        return visits;
     }
 
     public String getFIO() {
